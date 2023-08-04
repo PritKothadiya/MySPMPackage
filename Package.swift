0b7085a2496b9895225d421a12e47f4dde0f5ab4
PritKothadiya <pritkothadiya@gmail.com>
2023-08-04 16:50:37 +0530

    Commit for 0.0.4

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "MySPMPackage",
-    platforms: [.iOS(.v13)],
+    platforms: [.iOS("13.0")],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
@@ -18,6 +18,6 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .binaryTarget(name: "MySPMPackageFramework", path: "./Sources/MySPMPackageFramework.xcframework")
+        .binaryTarget(name: "MySPMPackage", path: "./Sources/MySPMPackage.xcframework")
     ]
 )
