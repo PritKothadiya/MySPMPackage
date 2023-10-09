ec88bf6d7136e819da9379bab7b70d679edf8c91
PritKothadiya <pritkothadiya@gmail.com>
2023-10-09 16:10:10 +0530

    - New release 0.0.6

--- Package.swift
+++ Package.swift
@@ -18,6 +18,8 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .binaryTarget(name: "MySPMPackage", path: "./Sources/MySPMPackage.xcframework")
+//        .binaryTarget(name: "MySPMPackage", path: "./Sources/MySPMPackage.xcframework")
+        
+        .binaryTarget(name: "MySPMPackage", url: "https://github.com/PritKothadiya/MySPMPackage/releases/download/0.0.6/MySPMPackage.xcframework.zip", checksum: "de17e3cb3bfbc1f2f310b1e2eb1d06800f898128d7426765638a3d4f9e0a79c7")
     ]
 )
